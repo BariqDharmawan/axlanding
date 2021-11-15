fafb7e1ae102bfb2e54fdbec9112ef606002979b
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-11-15 15:10:42 +0700

    [ADD] setup frontend asset

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+    <header class="bg-gradient-pink-to-red-bottom p-5 h-screen hero-darken-red">
+        <img src="{{ asset('img/city-left.png') }}" alt="" srcset="">
+    </header>
+@endsection
